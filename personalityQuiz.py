9d1e31dfcad13da548eee9b2cbcaaa3b5f3a5827
AmyCraven <92314129+AmyCraven@users.noreply.github.com>
2023-11-28 12:23:24 +0000

    Update personalityQuiz.py
    Signed-off-by: AmyCraven <92314129+AmyCraven@users.noreply.github.com>

--- personalityQuiz.py
+++ personalityQuiz.py
@@ -8,10 +8,13 @@ class PersonalityQuiz(QWidget):
     
     def __init__(self, social_questions, hobby_questions, selfcare_questions, organization_questions,muudy_window):
         super().__init__()
-        self.muudy_window = muudy_window
 
+        # Initialize attributes
+        self.muudy_window = muudy_window
         self.happiness_values = []
         self.current_page_index = 0
+
+        # Mapping of personality categories to types
         self.personality_types = {
             'Social': 'Diplomat',
             'Hobbies': 'Explorer',
@@ -19,8 +22,10 @@ class PersonalityQuiz(QWidget):
             'Organization': 'Sentinel'
         }
 
+        # Initialize the activity tracker
         self.activity_tracker = ActivityTracker(self)
 
+        # Descriptions of different personality types
         self.descriptions = {
             'Diplomat': "Diplomats are empathetic mediators, driven by ideals and harmony. They excel in understanding emotions, fostering connections, and navigating conflicts with grace.",
             'Explorer': "Explorers are adventurous creators, embracing spontaneity and excitement. They live in the present, adapting to change effortlessly, and are resourceful problem solvers with a zest for new experiences.",
@@ -28,28 +33,34 @@ class PersonalityQuiz(QWidget):
             'Sentinel': "Sentinels are practical organizers, prioritizing stability and order. They thrive on responsibility, reliability, and attention to detail, ensuring efficiency and a secure environment."
         }
 
+        # Set question attributes
         self.social_questions = social_questions
         self.hobby_questions = hobby_questions
         self.selfcare_questions = selfcare_questions
         self.organization_questions = organization_questions
 
+        # Initialize the user interface
         self.init_ui()
     
     def init_ui(self):
-
-
+        """
+        Initializes the user interface for the Muudy's personality quiz application.
+        """
+        # Start Page
         self.start_page = QWidget(self)
         start_layout = QVBoxLayout(self.start_page)
         start_layout.addLayout(self.create_button_layout())
-        welcome_label = QLabel("Welcome to Muudy's personality quiz! \nPlease answer the following questions by providing how happy the make you \nfeel using the slider")
+        welcome_label = QLabel("Welcome to Muudy's personality quiz!\n"
+                               "Please answer the following questions by providing how happy they make you\n"
+                               "feel using the slider")
 
         begin_button = QPushButton("Begin Quiz")
         begin_button.clicked.connect(self.start_questionnaire)
 
         start_layout.addWidget(welcome_label)
- 
         start_layout.addWidget(begin_button)
 
+        # Stacked Widget for Categories
         self.stacked_widget = QStackedWidget(self)
         self.categories = ['Social', 'Hobbies', 'Self-care', 'Organization']
         question_lists = [self.social_questions, self.hobby_questions, self.selfcare_questions, self.organization_questions]
@@ -57,15 +68,15 @@ class PersonalityQuiz(QWidget):
         self.stacked_widget.addWidget(self.start_page)
 
         for category, questions in zip(self.categories, question_lists):
+            # Page for each category
             page_widget = QWidget(self)
             page_layout = QVBoxLayout(page_widget)
             page_layout.addLayout(self.create_button_layout())
 
-
             page_layout.addWidget(QLabel(f'{category} Category'))
 
+            # Add sliders for each question
             for i, question in enumerate(questions, 1):
-
                 slider_label = QLabel(f'{i}: {question}')
                 slider = QSlider(Qt.Orientation.Horizontal)  # Horizontal orientation
                 slider.setMinimum(1)  # Set the minimum value to 1
@@ -79,52 +90,72 @@ class PersonalityQuiz(QWidget):
 
         self.stacked_widget.setCurrentIndex(0)
 
+        # Result Page
         self.result_label = QLabel()
         layout = QVBoxLayout()
         layout.addWidget(self.stacked_widget)
 
-
         self.personality_label = QLabel()
         layout.addWidget(self.personality_label)
 
+        # Next Button
         self.next_button = QPushButton('Next')
         if self.stacked_widget.currentIndex() == 0:
             self.next_button.setVisible(False)
         self.next_button.clicked.connect(self.next_category)
 
-         # Create a button for navigating to the Activity Tracker
+        # Activity Tracker Button
         self.at_button = QPushButton('Go to Activity Tracker')
         self.at_button.setObjectName('atButton')
         self.at_button.clicked.connect(self.show_activity_tracker)
+        self.stacked_widget.addWidget(self.at_button)
 
+        self.stacked_widget.addWidget(self.activity_tracker)
         self.results_page = QWidget(self)
         results_layout = QVBoxLayout(self.results_page)
         results_layout.addWidget(self.result_label)
         results_layout.addWidget(self.next_button)
         results_layout.addWidget(self.at_button)
+        self.at_button.hide()
 
         self.setLayout(layout)
-        self.setFixedSize(600, 400)
 
-
+        # Set window properties
         self.setGeometry(300, 300, 400, 300)
         self.setWindowTitle('Happiness Tracker')
         self.show()
 
     def start_questionnaire(self):
-        self.stacked_widget.setCurrentIndex(1)  # Start with the first category (index 1)
-        self.next_button.setVisible(True)  # Show the "Next" button on the category questions pages
+        """
+        Starts the personality questionnaire by displaying the questions for the first category.
+        """
+        # Set the current index of the stacked widget to the first category
+        self.stacked_widget.setCurrentIndex(1)
+        
+        # Show the "Next" button on the category questions pages
+        self.next_button.setVisible(True)
+        
+        # Add the "Next" button to the layout
         self.layout().addWidget(self.next_button)
-
+    
     def next_category(self):
+        """
+        Moves to the next category in the personality questionnaire or shows results if all categories are completed.
+        """
+        # Check if there are more categories to display
         if self.current_page_index < len(self.categories) - 1:
+            # Move to the next category
             self.current_page_index += 1
             self.stacked_widget.setCurrentIndex(self.current_page_index + 1)  # Adjust for the welcome page
         else:
+            # Show results if all categories are completed
             self.stacked_widget.setCurrentIndex(len(self.categories) + 1)  # Adjust for the welcome page
             self.show_results()
 
     def go_home(self):
+        '''
+        opening the home page
+        '''
         self.muudy_window.admin_button.show()
         self.muudy_window.member_button.show()
         self.muudy_window.guests_button.show()
@@ -134,7 +165,6 @@ class PersonalityQuiz(QWidget):
         """
         Switch to the Activity Tracker page within the PersonalityQuiz widget.
 
-
         This method hides the results page, hides the 'Next' button,
         sets the current index of the stacked widget to the Activity Tracker page,
         and applies the stylesheet for the Activity Tracker.
@@ -142,20 +172,21 @@ class PersonalityQuiz(QWidget):
         # Get the index of the Activity Tracker page in the stacked widget
         activity_tracker_index = self.stacked_widget.indexOf(self.activity_tracker)
 
-
         # Hide the results page and 'Next' button
         self.results_page.hide()
         self.next_button.hide()
 
-
         # Set the current index to the Activity Tracker page
         self.stacked_widget.setCurrentIndex(activity_tracker_index)
 
-
-        # Apply the stylesheet for the Activity Tracker
-        # self.activity_tracker.apply_stylesheet()
-
     def show_results(self):
+        """
+        Collects happiness values from the questionnaire and displays the personality quiz results.
+
+        Returns:
+        - None
+        """
+        # Collect happiness values from the questionnaire
         for i in range(self.stacked_widget.count()):
             page = self.stacked_widget.widget(i)
             for j in range(page.layout().count()):
@@ -163,20 +194,23 @@ class PersonalityQuiz(QWidget):
                 if isinstance(item.widget(), QSlider):
                     self.happiness_values.append(item.widget().value())
 
+        # Check if all prompts have the lowest value
         if all(score == 1 for score in self.happiness_values):
-            self.result_label.setText("This site may not be for you. You don't seem to fit into any of our categories.\nIf you'd like to carry on to our activity tracker, feel free, but it may be inaccurate.")
+            self.result_label.setText("This site may not be for you. You don't seem to fit into any of our categories.\n"
+                                      "If you'd like to carry on to our activity tracker, feel free, but it may be inaccurate.")
             personality_type = ""
             self.stacked_widget.addWidget(self.results_page)
             self.stacked_widget.setCurrentWidget(self.results_page)
             return
-    
 
-        # Check if all prompts are at the highest value
+        # Check if all prompts have the highest value
         if all(score == 10 for score in self.happiness_values):
             self.result_label.setText("You have a consistently positive and enthusiastic approach to all aspects of life! "
-                                    "You find maximum happiness in every category. \n If you'd like to be more specific \nfeel free to take the test again...")
-            personality_type = 'Optimist'
+                                      "You find maximum happiness in every category. \n If you'd like to be more specific "
+                                      "\nfeel free to take the test again...")
+            personality_type = ''
         else:
+            # Calculate scores for each category and identify the personality type
             category_scores = {}
             max_question_scores = {}
 
@@ -188,7 +222,8 @@ class PersonalityQuiz(QWidget):
             personality_type = self.personality_types[max_category]
 
             # Check if there are two categories with the same scores and the same max question scores
-            equal_categories = [self.personality_types[cat] for cat, score in category_scores.items() if score == category_scores[max_category] and max_question_scores[cat] == max_question_scores[max_category]]
+            equal_categories = [self.personality_types[cat] for cat, score in category_scores.items() if
+                                score == category_scores[max_category] and max_question_scores[cat] == max_question_scores[max_category]]
 
             if len(equal_categories) == 2:
                 personality_type = f'{equal_categories[0]} and {equal_categories[1]}'
@@ -199,16 +234,19 @@ class PersonalityQuiz(QWidget):
 
             self.result_label.setText(description)
 
+        # Show the results page and the Activity Tracker button
         self.stacked_widget.addWidget(self.results_page)
         self.stacked_widget.setCurrentWidget(self.results_page)
-
-    def reset_questionnaire(self):
-        self.stacked_widget.setCurrentIndex(1)  # Go back to the first question page
-        self.happiness_values = []  # Reset happiness values
-        self.next_button.setVisible(True)  # Show the "Next" button on the category questions pages
-        self.layout().addWidget(self.next_button)
+        self.at_button.show()
+        self.next_button.hide()
     
     def create_button_layout(self):
+        """
+        Creates a horizontal layout for buttons, including a 'Home' button.
+
+        Returns:
+        - QHBoxLayout: The created horizontal button layout.
+        """
         button_layout = QHBoxLayout()
         button_layout.addStretch(2)
 
@@ -231,7 +269,7 @@ if __name__ == '__main__':
     #     style_file.close()
 
     muudy_window = MuudyWindow()
-    # muudy_window.show()
+    muudy_window.show()
 
 
     sys.exit(app.exec())
